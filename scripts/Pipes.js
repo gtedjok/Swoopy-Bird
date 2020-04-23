6a6074f7361ae638c0bca79671680df235d7c4d5
katerinag <61842289+katerinag@users.noreply.github.com>
2020-04-22 20:50:08 -0400

    Update Pipes.js

--- scripts/Pipes.js
+++ scripts/Pipes.js
@@ -3,18 +3,33 @@ class Pipes {
   constructor() {
     this.x = windowWidth + 100;
     this.y = windowHeight + 100;
-    this.speed = 10;
+    this.randNum = Math.floor(Math.random() * 100); //random will return a double between 0 - 1
+    this.rand = Math.random() < 0.5 ? this.randNum : -this.randNum; //50% of rand number ot be pos or neg
+    this.speed = 8;
   }
 
-  drop() {
+  moveBack() {
     this.x -= this.speed; //falling
+  }
+
+  remove() {
+    if (this.x < 0){
+      return true;
+    }
+    else {
+      {
+        return false;
+      }
+    }
   }
 
   display() { //draw the catcher
     stroke(0);
     fill(0, 255, 0);
-    rect(this.x, 500, 55, 250);
-    rect(this.x, 0, 55, 250);
+    rect(this.x, 0, 55, 300 + this.rand); //top pipe
+    rect(this.x, 550 + this.rand, 55, windowHeight);
+
 
   }
 }
+
