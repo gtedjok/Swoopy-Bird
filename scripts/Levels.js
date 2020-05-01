147e4f5782267d206c75cf4f22d0312459a04070
katerinag <61842289+katerinag@users.noreply.github.com>
2020-04-30 21:59:59 -0400

    Update Levels.js
    We made the text easier to read.

--- scripts/Levels.js
+++ scripts/Levels.js
@@ -11,14 +11,14 @@ class Levels {
   display() {
     stroke(0);
     fill(255, 255, 0);
-    textSize(100);
+    textSize(80);
     rect(screen.width/4 - this.levelSide/2, 300, this.levelSide, this.levelSide); //top pipe
     rect(screen.width/2 - this.levelSide/2, 300, this.levelSide, this.levelSide);
     rect(screen.width/4 * 3 - this.levelSide/2, 300, this.levelSide, this.levelSide); //top pipe
 
     fill(0, 0, 0);
 
-    text("Click the level you want to play", 30, 150);
+    text("Type 1, 2, or 3 to select level", 30, 150);
 
     text("1", screen.width/4 - 30, this.levelSide + 180);
     text("2", screen.width/2 - 30, this.levelSide + 180);
